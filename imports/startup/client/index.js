c727fb132ab065f98fb848bcd76e89c4a5bcceef
Brady Wright <Pro75357@gmail.com>
2018-04-06 16:27:06 -0700

    First big changes
    Towards a flat HTML version

--- imports/startup/client/index.js
+++
@@ -1,4 +0,0 @@
-// Import client startup through a single index entry point
-
-import './routes.js'
-import './shareit_config.js'
--- imports/startup/server/register-api.js
+++
@@ -1,4 +0,0 @@
-// Register your apis here
-
-import '../../api/links/links.js'
-import '../../api/tweets.js'
